e84c89e634f024c64ad9846aad48888e05e1ed9b
rclemsmith <rclemsmith@gmail.com>
2020-06-14 12:28:06 +0530

    Mobile Auth Added

--- frontend/src/components/vbjComponents/LoginView.js
+++ frontend/src/components/vbjComponents/LoginView.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { Jumbotron, FormGroup, Form, FormText, Label, Input, Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import { AvForm, AvField } from 'availity-reactstrap-validation';
 import firebase from '../../config/firebase';
@@ -6,81 +6,99 @@ import { Redirect } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
 
-class LoginView extends Component{
-    constructor(props){
+class LoginView extends Component {
+    constructor(props) {
         super(props);
 
-        this.state={
-            
+        this.state = {
+
             otp: '',
             success: false,
             alert: false,
             disabled: true
         }
         this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);        
 
 
 
     }
 
-    handleClick(e){
-        
-        let recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha');
-        let number = '+91'+this.phoneno.value;
-        firebase.auth().signInWithPhoneNumber(number,recaptcha)
-            .then(function(e){
-                this.setState({disabled: false})
-                let code = prompt('enter the otp','');
-                if(code == null) return;
-                e.confirm(code)
+    setUpRecaptcha = () => {
+        window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+            'size': 'invisible',
+            'callback': function (response) {
+                // reCAPTCHA solved, allow signInWithPhoneNumber.
+                // this.onSignInSubmit();
+            }
+        });
+    }
+
+    onSignInSubmit = (event) => {
+        var phoneNumber = "+917550145401";
+        this.setUpRecaptcha();
+        var appVerifier = window.recaptchaVerifier;
+        console.log("Indie Onsub")
+        firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+            .then(function (confirmationResult) {
+                // SMS sent. Prompt user to type the code from the message, then sign the
+                // user in with confirmationResult.confirm(code).
+                window.confirmationResult = confirmationResult;
+                console.log("OTP Send");
+                let code = prompt('enter the otp', '');
+                if (code == null) return;
+                confirmationResult.confirm(code)
                     .then((result) => {
-                        console.log(result.user,'user');
-                    
+                        console.log(result.user, 'user');
+
                         window.alert("Number Verified");
                     })
                     .catch((err) => {
                         console.log(err);
                     })
-            })
+            }).catch(function (error) {
+                // Error; SMS not sent
+                // ...
+                console.log(error);
+            });
     }
+
     
 
-    handleChange(e){
+    handleChange(e) {
         this.setState({
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
-    toggleModal(){
+    toggleModal() {
         this.setState({
             isModalRegister: !this.state.isModalRegister
         });
     }
 
 
-    render(){
+    render() {
 
-        if(this.state.success){
-            return <Redirect to={ROUTES.DASHBOARD}/>
+        if (this.state.success) {
+            return <Redirect to={ROUTES.DASHBOARD} />
         }
-        if(this.state.alert){
+        if (this.state.alert) {
             window.alert("Invalid User");
         }
-        
-        return(
 
-            <div>
-                <input type="text" id="otp" innerRef={(input) => this.phoneno=input} />
-                <button onClick={this.handleClick} id="recaptcha">Verify</button>
+        return (
+
+            <div >
+                <div id="recaptcha-container"></div>
+                <button onClick={this.onSignInSubmit}>Verify</button>
                 
             </div>
-            
-            
-           
-            
 
-        
+
+
+
+
+
         );
     }
 }
--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket : process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId:process.env.REACT_APP_APP_ID,
-    measurementId:process.env.REACT_APP_MEASUREMENT_ID
-};
+    apiKey: "AIzaSyCgMfU9aS6h8qjAF8NxzBHhTS84palI7UY",
+    authDomain: "covid-hack-5267d.firebaseapp.com",
+    databaseURL: "https://covid-hack-5267d.firebaseio.com",
+    projectId: "covid-hack-5267d",
+    storageBucket: "covid-hack-5267d.appspot.com",
+    messagingSenderId: "1017511849431",
+    appId: "1:1017511849431:web:cc93ae0b60b3090cfaef92",
+    measurementId: "G-N63SSP8GL1"
+  }
 
 firebase.initializeApp(config);
 
