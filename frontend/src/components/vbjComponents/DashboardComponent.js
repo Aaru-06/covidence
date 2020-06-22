f5709951d691ae9ca50cd4e5655728bb0c29382b
Aaru-06 <arumugampillai0610@gmail.com>
2020-06-22 12:22:49 +0530

    side nav bar added

--- frontend/src/components/vbjComponents/DashboardComponent.js
+++ frontend/src/components/vbjComponents/DashboardComponent.js
@@ -3,12 +3,14 @@ import modules from '../../constants/Modules';
 import Header from './HeaderView';
 import DashboardModuleComponent from '../arumugamComponents/DashboardModuleComponent';
 import img from "../../images/covid.jpg"
+import SideNavBar from "../arumugamComponents/SideNavBar"
 const Dashboard = props  => {
     // console.log("dashboard rendered")
     // console.log(modules)
     return(
         <div>
             <Header name="Dashboard" />
+            <SideNavBar/>
             <div className="moduleContainer">
                 {/* <h1>{props.user}</h1> */}
                 {
