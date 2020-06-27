1e6c3b643d2dbd9ce51322706a8bd05e4ce1ed57
Aaru-06 <arumugampillai0610@gmail.com>
2020-06-27 21:02:36 +0530

    Updates

---
+++ precache-manifest.55742b6d5587b9fd1d07359333a9bc08.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516772eb95232d7de3f8a0dd1bf843e0",
+    "url": "/covidence/frontend/index.html"
+  },
+  {
+    "revision": "697c407424524c10702d",
+    "url": "/covidence/frontend/static/css/2.9c328e0b.chunk.css"
+  },
+  {
+    "revision": "c2943beb6f6e5630399a",
+    "url": "/covidence/frontend/static/css/main.6f89586e.chunk.css"
+  },
+  {
+    "revision": "697c407424524c10702d",
+    "url": "/covidence/frontend/static/js/2.0d70aa8b.chunk.js"
+  },
+  {
+    "revision": "58a4f9cbf2f7cf15b97e546e5fc4cb17",
+    "url": "/covidence/frontend/static/js/2.0d70aa8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2943beb6f6e5630399a",
+    "url": "/covidence/frontend/static/js/main.da6c2dfb.chunk.js"
+  },
+  {
+    "revision": "d27599462a4077fbf0ce",
+    "url": "/covidence/frontend/static/js/runtime-main.4c4c3641.js"
+  },
+  {
+    "revision": "1aef562d0e88f08f3c745436828f1d80",
+    "url": "/covidence/frontend/static/media/covid.1aef562d.jpg"
+  },
+  {
+    "revision": "a78e51e0bf277068c25aa1a49a504f52",
+    "url": "/covidence/frontend/static/media/covid191.a78e51e0.jpeg"
+  },
+  {
+    "revision": "bc477d0dd826674f795f2f7bf022d64b",
+    "url": "/covidence/frontend/static/media/shop.bc477d0d.jpg"
+  }
+]);
