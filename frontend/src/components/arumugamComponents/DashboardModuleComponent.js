073ece8b473fa3f1bf49df3de0990bfb0910dab4
Vbj <ijalab1808@gmail.com>
2020-06-27 19:18:13 +0530

    added logout

--- frontend/src/components/arumugamComponents/DashboardModuleComponent.js
+++ frontend/src/components/arumugamComponents/DashboardModuleComponent.js
@@ -1,23 +1,31 @@
-import React,{Component} from "react"
+import React, { Component } from "react";
 
-class DashboardModuleComponent extends Component{
+class DashboardModuleComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.clickHandler = this.clickHandler.bind(this);
+  }
 
-    constructor(props){
-        super(props);
-        this.clickHandler = this.clickHandler.bind(this);
+  clickHandler(event) {
+    const name = event.target.id;
+    console.log(name);
+    if (name == "logout") {
+      this.props.history.push("/");
+    } else {
+      this.props.history.push(`/${name}`);
     }
-
-    clickHandler(event){
-        let name = event.target.id;
-        this.props.history.push(`/${name}`);
-    }
-    render(){
-        return(
-            <div className="moduleComponent" id={this.props.name} onClick={this.clickHandler}>
-                {this.props.name}
-            </div>
-        )
-    }
+  }
+  render() {
+    return (
+      <div
+        className="moduleComponent"
+        id={this.props.name}
+        onClick={this.clickHandler}
+      >
+        {this.props.name}
+      </div>
+    );
+  }
 }
 
-export default DashboardModuleComponent
+export default DashboardModuleComponent;
