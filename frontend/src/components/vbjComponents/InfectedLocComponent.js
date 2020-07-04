469d99b61e6f3364d3d94357489910dc5ee357b9
Vbj <ijalab1808@gmail.com>
2020-07-04 19:50:25 +0530

    modified infectedloc

--- frontend/src/components/vbjComponents/InfectedLocComponent.js
+++ frontend/src/components/vbjComponents/InfectedLocComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import firebase from "../../config/firebase";
 import Header from "./HeaderView";
 import "./styles/styles.css";
@@ -11,10 +11,18 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-const Infected = (props) => {
-  var [infect, setInfect] = useState("");
-  var address = [];
-  useEffect(() => {
+class Infected extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      infect: "",
+      address: "",
+    };
+  }
+
+  componentDidMount() {
+    let address = [];
+    let infectedloc = this;
     let ref = firebase.database().ref();
     ref.child("Infected").on("value", (snapshot) => {
       // if (snapshot.child("addr").val != null) {
@@ -25,22 +33,31 @@ const Infected = (props) => {
       snapshot.forEach(function (childSnapshot) {
         //Here you can access  childSnapshot.key
         childSnapshot.forEach((snap) => {
+          console.log(snap.child("addr").val());
+          console.log(snap.child("dateAndTime").val());
           address.push(snap.child("addr").val());
-          setInfect({ ...snap.child("dateAndTime").val() });
+
+          infectedloc.setState({
+            infect: snap.child("dateAndTime").val(),
+            address: snap.child("addr").val(),
+          });
         });
       });
     });
-  }, []);
-  console.log(address);
-  console.log(infect);
+    console.log(address);
+    console.log(this.state.address);
+    console.log(this.state.infect);
+  }
 
-  return (
-    <>
-      <Header name="Infected Location" />
-      <SideNavBar history={props.history} />
-      <div className="row"></div>
-    </>
-  );
-};
+  render() {
+    return (
+      <>
+        <Header name="Infected Location" />
+        <SideNavBar history={this.props.history} />
+        <div className="row"></div>
+      </>
+    );
+  }
+}
 
 export default Infected;
